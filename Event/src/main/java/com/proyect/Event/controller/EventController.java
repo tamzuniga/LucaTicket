78771d0625604c8b3aa895fbaf5821be126ded8d
G0onzal0 <monederogonzalo@gmail.com>
2022-01-25 17:50:29 +0100

    Controller

--- Event/src/main/java/com/proyect/Event/controller/EventController.java
+++ Event/src/main/java/com/proyect/Event/controller/EventController.java
@@ -1,16 +1,33 @@
 package com.proyect.Event.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.proyect.Event.Services.EventServices;
 import com.proyect.Event.adapter.EventAdapter;
+import com.proyect.Event.model.Event;
 import com.proyect.Event.response.EventResponse;
 
+@RestController
+/*
+@RequestMapping("/event")
+*/
 public class EventController {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(EventController.class);
+	
 	@Autowired
 	private EventServices eventServices;
 	
@@ -22,5 +39,19 @@ public class EventController {
 	public List<EventResponse> findAll(){
 		
 		return eventServices.findAll();
+	}
+	
+	@PutMapping
+	public Event save(Event event) {
+		return eventServices.save(event);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> addEvent(@RequestBody Event event) {
+		Event result = this.save(event);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{code}").buildAndExpand(result.getCode())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
