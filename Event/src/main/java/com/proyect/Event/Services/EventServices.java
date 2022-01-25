8cfc4dd5d53a85d894bef9e9f9892aa39f4d1bac
alumno <alumno@Alumno07>
2022-01-25 14:29:53 +0100

    Que no pete por Dios

--- Event/src/main/java/com/proyect/Event/Services/EventServices.java
+++ Event/src/main/java/com/proyect/Event/Services/EventServices.java
@@ -1,9 +1,16 @@
 package com.proyect.Event.Services;
 
 
+import java.util.List;
+
 import com.proyect.Event.model.Event;
+import com.proyect.Event.response.EventResponse;
 
 public interface EventServices {
 
 	public Event save(Event event);
+	
+	//Listar Eventos
+	
+	public List<EventResponse> findAll();
 }
--- Event/src/main/java/com/proyect/Event/Services/EventServicesImp.java
+++ Event/src/main/java/com/proyect/Event/Services/EventServicesImp.java
@@ -1,9 +1,13 @@
 package com.proyect.Event.Services;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.proyect.Event.adapter.EventAdapter;
 import com.proyect.Event.model.Event;
 import com.proyect.Event.repository.EventRepository;
+import com.proyect.Event.response.EventResponse;
 
 @Service
 public class EventServicesImp implements EventServices{
@@ -11,9 +15,21 @@ public class EventServicesImp implements EventServices{
 	@Autowired
 	EventRepository er;
 	
+	@Autowired
+	EventAdapter eventAdapter;
+	
 	@Override
 	public Event save(Event event) {
 		
 		return er.save(event);
 	}
+
+	@Override
+	public List<EventResponse> findAll() {
+		final List<Event> all = er.findAll();
+		return eventAdapter.of(all);
+	}
+
+	//Listar eventos
+	
 }
