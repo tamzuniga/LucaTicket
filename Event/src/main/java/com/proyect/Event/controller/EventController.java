452f7d38937b2e87cb38a76c679e482e4efa0967
juan sebastian quinayas guarin <sebastianquemasda@gmail.com>
2022-01-26 12:30:04 +0100

    open api for events

--- Event/src/main/java/com/proyect/Event/controller/EventController.java
+++ Event/src/main/java/com/proyect/Event/controller/EventController.java
@@ -20,58 +20,64 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import com.proyect.Event.Services.EventServices;
 import com.proyect.Event.adapter.EventAdapter;
 import com.proyect.Event.model.Event;
 import com.proyect.Event.response.EventResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 
 @RequestMapping("/event")
+@Tag(name = "Event", description = "The event API")
 public class EventController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(EventController.class);
-	
+
 	@Autowired
 	private EventServices eventServices;
-	
+
 	@Autowired
 	private EventAdapter eventAdapter;
-	
 
+	@Operation(summary = "List of all available events", description = "returns a json with all events in the database", tags = {
+			"Event" })
 	@GetMapping("/events")
-	public List<EventResponse> findAll(){
-		
+	public List<EventResponse> findAll() {
+
 		return eventServices.findAll();
 	}
-	
+
+	@Operation(summary = "Save a event", description = "inserts an event to the database", tags = { "Event" })
 	@PutMapping
 	public Event save(Event event) {
 		return eventServices.save(event);
 	}
-	
+
+	@Operation(summary = "Add a new eventt", description = "Add a new eventt", tags = { "Event" })
 	@PostMapping("/add")
 	public ResponseEntity<?> addEvent(@RequestBody Event event) {
 		Event result = this.save(event);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{code}").buildAndExpand(result.getCode())
 				.toUri();
-		
-		return ResponseEntity.created(location).build();
-  }
 
+		return ResponseEntity.created(location).build();
+	}
+
+	@Operation(summary = "Delete a event", description = "Deletes an event by its code", tags = { "Event" })
 	@DeleteMapping("/deleteEvent/{code}")
 	public void deleteUser(@PathVariable String code) {
 		eventServices.deleteEvent(code);
 
 	}
 
-	//Actualizar juego
-		@PutMapping("/{code}")
-		public void updateGame(@RequestBody Event event, @PathVariable String code) {
-			
-				eventServices.deleteEvent(code);
-				eventServices.save(event);
-					
-		}
+	// Actualizar juego
+	@Operation(summary = "Update event", description = "Update an event by its code", tags = { "Event" })
+	@PutMapping("/{code}")
+	public void updateGame(@RequestBody Event event, @PathVariable String code) {
+		eventServices.deleteEvent(code);
+		eventServices.save(event);
+	}
 }
