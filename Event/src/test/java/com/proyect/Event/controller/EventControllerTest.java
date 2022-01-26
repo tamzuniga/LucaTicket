d341973660f2032cb2e15fa1bdaa9c8bbe636838
G0onzal0 <monederogonzalo@gmail.com>
2022-01-26 08:59:15 +0100

    Test

---
+++ Event/src/test/java/com/proyect/Event/controller/EventControllerTest.java
@@ -0,0 +1,34 @@
+package com.proyect.Event.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EventControllerTest {
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateGame() {
+		fail("Not yet implemented");
+	}
+
+}
